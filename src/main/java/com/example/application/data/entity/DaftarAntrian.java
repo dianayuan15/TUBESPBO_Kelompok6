3e5209a5eaa6ec5b9ac966a748151e9b85a9a131
xxsitiafrida <sitiafrida1234@gmail.com>
2023-01-09 00:27:43 +0700

    Perbaikan Daftar Antrian

--- src/main/java/com/example/application/data/entity/DaftarAntrian.java
+++ src/main/java/com/example/application/data/entity/DaftarAntrian.java
@@ -20,10 +20,12 @@ public class DaftarAntrian extends AbstractEntity {
     public void setNik(String Nik) {
         this.nik = nik;
     }
+
     public String getNama() {return nama;}
     public void setNama(String nama) {
         this.nama = nama;
     }
+
     public String getNmrTlpn() {return nmrTlpn;}
     public void setNmrTlpn(String nmrTlpn) {
         this.nmrTlpn = nmrTlpn;
@@ -32,24 +34,31 @@ public class DaftarAntrian extends AbstractEntity {
     public LocalDate getTanggalLahir() {
         return tanggalLahir;
     }
+
     public void setTanggalLahir(LocalDate tanggalLahir) {
         this.tanggalLahir = tanggalLahir;
     }
+
     public String getJenisKelamin() {
         return jenisKelamin;
     }
+
     public void setJenisKelamin(String jenisKelamin) {
         this.jenisKelamin = jenisKelamin;
     }
+
     public String getAlamat() {
         return alamat;
     }
+
     public void setAlamat(String alamat) {
         this.alamat = alamat;
     }
+
     public String getKeluhan() {
         return keluhan;
     }
+
     public void setkeluhan(String keluhan) {
         this.keluhan = keluhan;
     }
